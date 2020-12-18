ab5e61b823aa832ef976050a06e0d6f02d7ab475
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2020-12-18 17:03:03 -0300

    Binding to Custom Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{ type: 'server', name: 'Test', content: 'Just a test.' }];
 
+  onAddServer(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onAddBlueprint(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
 }
